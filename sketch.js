eaf310a04f8f4cf439906c9d1811e686d267ee0b
w06148eb <elliot.brooks@student.manchester.ac.uk>
2022-01-23 08:50:00 +0000

    Visualise Tangents

--- sketch.js
+++ sketch.js
@@ -1,16 +1,25 @@
 function setup() {
   createCanvas(800, 800);
+
+  // points
   vertex1 = new CurveVertex(100,400, 15, 15);
   vertex2 = new CurveVertex(700,400, 15, 15);
   controlPoint1 = new CurveVertex(200,300, 15, 15);
   controlPoint2 = new CurveVertex(600,500, 15, 15);
 
-  delta = 0.1;
 
+  // visualisation parameters
+  cubicBool = createCheckbox('Cubic?');
+  tangentVisualiser = createCheckbox('Visualise Tangents');
+  Tslider = createSlider(0.01, 0.5, 0.05,0.005);
 }
 
 function draw() {
-  background(220);
+  noStroke();
+  background(0);
+
+
+  delta = Tslider.value();
 
   vertex1.over();
   vertex1.update();
@@ -20,22 +29,36 @@ function draw() {
   vertex2.update();
   vertex2.show();
 
+  if (cubicBool.checked()) {
+    controlPoint2.over();
+    controlPoint2.update();
+    controlPoint2.show();
+  }
   controlPoint1.over();
   controlPoint1.update();
   controlPoint1.show();
 
-  controlPoint2.over();
-  controlPoint2.update();
-  controlPoint2.show();
 
+
+  // draw points acording to t
   noFill();
+  stroke(255);
   beginShape();
-  for (t = 0; t < 1.00001; t += delta) {
-    let points = quadraticBezier(t, vertex1, controlPoint1, vertex2);
+
+  for (t = 0; t <= 1.00000001; t += delta) {
+    if (!cubicBool.checked()) {
+      var points = quadraticBezier(t, vertex1, controlPoint1, vertex2);
+    }
+    else{
+      var points = quadraticBezier(t, vertex1, controlPoint1, vertex2);
+    }
     vertex(points[0], points[1]);
   }
+  vertex(vertex2.x,vertex2.y);
   endShape();
-  //bezier(vertex1.x, vertex1.y, controlPoint1.x,controlPoint1.y ,controlPoint2.x,controlPoint2.y,vertex2.x,vertex2.y);
+
+
+
 }
 
 
@@ -58,8 +81,24 @@ function mouseReleased() {
 function quadraticBezier(t, anchor1, control, anchor2) {
   let x = ((1 - t)**2 * anchor1.x) + 2*(1 - t)*t*control.x + (t**2)*anchor2.x;
   let y = ((1 - t)**2 * anchor1.y) + 2*(1 - t)*t*control.y + (t**2)*anchor2.y;
+
+  if (tangentVisualiser.checked()) {
+    let visualiseX = lerp(anchor1.x, controlPoint1.x, t);
+    let visualiseY = lerp(anchor1.y, controlPoint1.y, t);
+    let visualiseX2 = lerp(controlPoint1.x, anchor2.x, t);
+    let visualiseY2 = lerp(controlPoint1.y, anchor2.y, t);
+    colorMode(HSB);
+    stroke(t*360, 255, 255, 128);
+    line(visualiseX,visualiseY,visualiseX2,visualiseY2);
+
+  }
+
+
+
   return [x,y];
 }
 
 function cubicBezier(t, anchor1, control1, control2, anchor2) {
 }
+
+//bezier(vertex1.x, vertex1.y, controlPoint1.x,controlPoint1.y ,controlPoint2.x,controlPoint2.y,vertex2.x,vertex2.y);
